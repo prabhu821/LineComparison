5e21c834a832577ebe645cd1205f21491d7c745f
rishiraj007 <rishiraj@mounttalent.com>
2023-06-09 11:08:02 +0530

    [Prabhu]Add: Calculate length

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem !");
+            LineComparison lineComparison = new LineComparison(8, 6, 4, 2);
+            lineComparison.CalculateLength();
         }
     }
 }
